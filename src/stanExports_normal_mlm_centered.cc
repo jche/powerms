99dc184b3778e48b0af2daf2b9c1097a0edda952
jche <jonche10@gmail.com>
2023-06-30 14:41:30 -0400

    a bunch of changes happened upon running devtools::load_all()... unsure what to make of these

--- src/stanExports_normal_mlm_centered.cc
+++ src/stanExports_normal_mlm_centered.cc
@@ -7,7 +7,7 @@ using namespace Rcpp ;
 RCPP_MODULE(stan_fit4normal_mlm_centered_mod) {
 
 
-    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("rstantools_model_normal_mlm_centered")
+    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("model_normal_mlm_centered")
 
     .constructor<SEXP,SEXP,SEXP>()
 
